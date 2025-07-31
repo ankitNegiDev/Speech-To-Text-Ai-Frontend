1ac779ef026d5761dde6e99ef16b517a1727faaa
ankitNegiDev <ankitnegi9104@gmail.com>
2025-07-31 20:15:48 +0530

    updated the backend deployed api and added sample audio and added animation in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,42 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
+import Transcribe from './pages/Transcribe'
+import History from './pages/History'
+import Layout from './components/Layout'
+import { Toaster } from 'react-hot-toast';
+
 
 function App() {
 
     return (
         <>
-            {/* <div className='bg-blue-300 text-3xl font-bold text-center'>Hello this is App comonent</div> */}
-            <div>
-                <Routes>
-                    <Route path='/' element={<Home/>} />
-                    <Route path='/dashboard' element={<Dashboard/>} />
-                </Routes>
-            </div>
+            <Toaster position="top-center" reverseOrder={false} />
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    {/* <Route path="dashboard" element={<Dashboard />} /> */}
+                    <Route path="transcribe" element={<Transcribe />} />
+                    <Route path="history" element={<History />} />
+                </Route>
+            </Routes>
         </>
+    );
 
-    )
+    // return (
+    //     <>
+    //         {/* <div className='bg-blue-300 text-3xl font-bold text-center'>Hello this is App comonent</div> */}
+    //         <div>
+    //             <Routes>
+    //                 <Route path='/' element={<Home/>} />
+    //                 <Route path='/dashboard' element={<Dashboard/>} />
+    //                 <Route path='/transcribe' element={<Transcribe />} /> 
+    //                 <Route path="/history" element={<History />} />
+    //             </Routes>
+    //         </div>
+    //     </>
+
+    // )
 }
 
 export default App
